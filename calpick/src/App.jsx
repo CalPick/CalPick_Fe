58218462a350c7672c5aada3bc852b4d4571d080
leesu <a91364090@gmail.com>
2025-07-07 20:14:59 +0900

    회원가입 및 로그인 페이지 구현

--- calpick/src/App.jsx
+++ calpick/src/App.jsx
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Signup from './Signup';
+import LoginSuccess from './LoginSuccess';  // 이 파일도 src에 추가할 예정
+
 function App() {
   return (
-    <div className="text-3xl font-bold underline text-red-600">
-      Hello Tailwind + Vite + React!
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/success" element={<LoginSuccess />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ calpick/src/LoginSuccess.jsx
@@ -0,0 +1,54 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
+function LoginSuccess() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const userId = location.state?.userId || "아이디 없음";
+  const password = location.state?.password || "";
+
+  const handleStart = async () => {
+    try {
+      const response = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ userId, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        localStorage.setItem("token", data.accessToken);
+        localStorage.setItem("nickname", data.nickname);
+        navigate("/"); //메인화면 주소
+      } else {
+        alert(data.error || "자동 로그인 실패");
+      }
+    } catch (error) {
+      alert("서버 오류");
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white">
+      <h1 className="text-2xl font-black mb-10">회원가입 완료!</h1>
+
+      <p className=" font-semibold ">
+        아이디: <span className="font-bold">{userId}</span>
+      </p>
+
+      <p className="m-10">
+        캘픽이 빠르게 약속을 잡아드릴게요.
+      </p>
+
+      <button
+        onClick={handleStart}
+        className="justify-center w-70 py-2 bg-black text-white font-bold rounded-xl"
+      >
+    시작하기
+      </button>
+    </div>
+  );
+}
+
+export default LoginSuccess;
