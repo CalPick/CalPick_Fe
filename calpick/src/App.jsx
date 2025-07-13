6fcffec3a809112c2f6740ae854104edfd9629f4
leesu <a91364090@gmail.com>
2025-07-13 22:24:06 +0900

    패키지 재설치

--- calpick/src/App.jsx
+++ calpick/src/App.jsx
@@ -1,13 +1,15 @@
-
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CalendarPage from './components/calendar/CalendarPage';
 import Login from './Login';
 import Signup from './Signup';
-import LoginSuccess from './LoginSuccess';  // 이 파일도 src에 추가할 예정
+import LoginSuccess from './LoginSuccess';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<CalendarPage userId={"yourUserId"} />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/success" element={<LoginSuccess />} />
