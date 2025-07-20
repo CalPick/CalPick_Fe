f1e0e84de827085399dabd2387862bda3039bdf0
leesu <a91364090@gmail.com>
2025-07-21 00:50:18 +0900

    FriendItem 수정

--- calpick/src/components/sidebar/FriendItem.jsx
+++ calpick/src/components/sidebar/FriendItem.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import calendarIcon from "../../assets/calendaricon.svg";
 
-function FriendItem({ friend }) {
+function FriendItem({ friend, onCalendarClick }) {
+  const { id, name } = friend;
+
+  const handleCalendarClick = () => {
+    if (onCalendarClick) {
+      onCalendarClick(id, name);
+    }
+  };
+
   return (
     <div className="flex justify-between items-center w-full h-[44px] px-[20px]">
-      <span className="text-[16px] font-[500]">{friend.name}</span>
+      <span className="text-[16px] font-[500]">{name}</span>
       <img
         src={calendarIcon}
         alt="calendaricon"
         className="w-[18px] h-[18px]"
+        onClick={handleCalendarClick}
       />
     </div>
   );
